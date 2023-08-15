44dc13586075d603c4a52cc97d2a687de220ef88
Rebun13 <rubengdopazo@gmail.com>
2023-08-15 09:37:12 +0100

    Added the code

---
+++ GridChallenge.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * The function is expected to return a STRING.
+ * The function accepts STRING_ARRAY grid as parameter.
+ */
+
+string gridChallenge(vector<string> grid) {
+    int n = grid.size();
+    for(int i = 0; i < n; i++) {
+        sort(grid[i].begin(), grid[i].end());
+        cout << ":" << grid[i] << endl;
+    }
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n-1; j++) 
+            if(grid[j][i] > grid[j + 1][i]) {
+                cout << grid[j][i] << " > " << grid[j + 1][i] << endl;
+                return "NO";
+            }
+    return "YES";
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<string> grid(n);
+
+        for (int i = 0; i < n; i++) {
+            string grid_item;
+            getline(cin, grid_item);
+
+            grid[i] = grid_item;
+        }
+
+        string result = gridChallenge(grid);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);})
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(),
+        s.end()
+    );
+
+    return s;
+}
